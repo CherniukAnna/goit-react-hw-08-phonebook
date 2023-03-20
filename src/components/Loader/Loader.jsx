0f88c56a953966bb0627dbcbb4a48faad1e4f65f
CherniukAnna <chernukanna@gmail.com>
2023-03-20 09:16:04 +0200

    fixed form

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ export const Loader = () => {
   return (
     <div className={s.loading}>
       <RotatingLines
-        strokeColor="rgba(178 146 171 / 50%)"
+        strokeColor="rgba(149, 99, 185, 0.661)"
         strokeWidth="5"
         animationDuration="0.75"
         width="50"
--- src/redux/store.js
+++ src/redux/store.js
@@ -32,9 +32,6 @@ export const store = configureStore({
       },
     }),
   ],
-  // devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
-
-// export { store, persistor };
